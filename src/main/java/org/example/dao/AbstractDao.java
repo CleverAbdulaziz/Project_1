6b85043b2757187dc6a9de3cd1722784de99eee0
Azimxon Toirov <@azimkhon_toirov>
2024-03-25 14:16:55 +0500

    commit

---
+++ src/main/java/org/example/dao/AbstractDao.java
@@ -0,0 +1,4 @@
+package org.example.dao;
+
+public abstract class AbstractDao<A> implements ApplianceDAO<A>{
+}
---
+++ src/main/java/org/example/service/ApplianceService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+public interface ApplianceService {
+    public static final int num = 500;
+}
+
+class Classing implements ApplianceService {
+    public static void main(String[] args) {
+
+    }
+}
